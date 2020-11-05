fd095caa9384a80f7a0b72928e76fdd96070ece4
awfaizzahazhar <72841382+awfaizzahazhar@users.noreply.github.com>
2020-11-05 12:46:48 +0800

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,47 +1,74 @@
-public class Circle 
-{
-	//field/attribute
-	private double jejari = 1;
+package SemuaCircle.CircleKecil;	
 
-	public void SetJejari(double Jejari)
-	{
-		this.jejari=jejari;
+public class Circle{
+	//field or attribute
+	private double jejari, x; //non-static field
+	public static int bilObjekWujud = 0; //static field
 
+
+	//default constructor
+	public Circle(){
+		//System.out.println("Call default constructor ");
+		jejari = 1;
+		x = 5;
+		bilObjekWujud++;
 	}
 
 
-	public double getJejari()
-	{
+	// overloading constructor - 1 parameter
+	public Circle(double jejari){
+		// this();	//call default constructor 
+		this.jejari = jejari;
+		x = 5;
+		// this.x = x;
+		// this(jejari, 59); //call constructor 2 parameter
+		bilObjekWujud++;
+		// System.out.println("Jejari = " + jejari  );
+	}
+
+	//overloading constructor - 2 parameter
+	public Circle(double jejari, double x){
+		this.jejari = jejari;
+		this.x = x;
+		bilObjekWujud++;
+		//System.out.println("Jejari = " + jejari + " String = " + n );
+	}
+
+
+
+
+
+
+
+	//overload method toString() //ni method
+	public String toString(){
+		String tentangCircle;
+		tentangCircle = "Bil objek ialah " + bilObjekWujud;
+		// tentangCircle =  "Jejari = " + jejari + "\nx=" + x + 
+		// "\nLuas adalah " + KiraLuas();
+		return tentangCircle;
+	}
+
+	//setter
+	public void setJejari(double jejari){
+		this.jejari = jejari;
+		System.out.println(jejari);
+	}
+
+	//getter
+	//takyah buat this.jejari cuz nnty lmbat(?)
+	public double getJejari(){
 		return jejari;
 	}
-//default constructor
-	public Circle()
-	{
-		System.out.println("Default constructor dipanggil");
-	}
-
-	public Circle (double r)
-	{
-		jejari = r;
-		System.out.println("Jejari = " - jejari);
-	}
-
-
-	public Circle (double r)
-	{
-		jejari = r;
-	}
-
-	public Circle (double r, String s)
-	{
-		jejari = r;
-		System.out.println("Jejari = " + jejari + "String =" +s);
-	}
-
 
 	//method
-	public double KiraLuas() 
-	{
-		return jejari*jejari* Math.PI;
+	public double KiraLuas(){
+		return getJejari() * getJejari() * Math.PI;
 	}
+
+	public static double UkurLilit (double j){
+		return 2 * Math.PI *  j ;
+	}
+
+	
 }
--- DriverCircle.java
+++ DriverCircle.java
@@ -1,18 +1,47 @@
-public class DriverCircle
-{
-	public static void main(String[] args)
-	{
-	Circle bulat = new Circle();
+import SemuaCircle.CircleKecil.Circle;	//import specific class from package
 
-	Circle bulat1 = new Circle(5);
+public class DriverCircle{
 
-	Circle bulat2 = new Circle(8, "DITP3133");
+	public static void main(String[] args){
+		Circle bulat = new Circle();	//instantiate object
+		System.out.println("Print apa? " + bulat);
+
+		Circle bulat1 = new Circle(76);	//instantiate object - 1 parameter
+		System.out.println("Print apa? " + bulat1);
+
+		Circle bulat2 = new Circle(8, 3);
+		System.out.println("Print apa? " + bulat2);
+
+		Circle bulat3 = new Circle(8, 3);
+		System.out.println("Print apa? " + bulat3);
+
+		System.out.println("Ukur lilit = " + UkurLilit(bulat3.getJejari()));
+		
+		System.out.println("Ukur lilit = " + Circle.UkurLilit(bulat3.getJejari()));
+
+		System.out.println("Luas ialah " + KiraLuas(bulat2));
+
+		//Method call
+		System.out.println("Papar menunjukkan " + Papar());
+
+		
+
+	}
 
 
-	System.out.println("Jejari bulat1 - " + bulat1.getJejari());
-	System.out.println("Jejari bulat - " + bulat.getJejari());
-	System.out.println("Jejari bulat2 - " + bulat.getJejari());
+//method static tht return a reference(object)
+	public static Circle Papar(){
+		int j = 5;
+		return new Circle(j); //create object
+	}
 
-	//System.out.format("Luas ialah %2.2f" , bulat.KiraLuas());
-}
+	//call reference static method tht accept as its parameter
+	public static double KiraLuas(Circle b){
+		return b.getJejari() * b.getJejari() * Math.PI;
+	}
+
+	public static double UkurLilit (double jejari){
+		return 2 * Math.PI *  jejari ;	//Math.pow(3);
+	}
+
 }
